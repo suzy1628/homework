ff7e9c962ae6849420fdc078db5194dd611dfdaf
suzy <suzy@users-MacBook-Pro-2.local>
2023-03-27 23:02:24 +0600

    homework

---
+++ bin/dart_application_homework.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_homework/dart_application_homework.dart' as dart_application_homework;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_homework.calculate()}!');
+}
---
+++ test/dart_application_homework_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_homework/dart_application_homework.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
